267d4fb0087e5536393a812105b8ff6f4a2e6bf9
verafilippova <vera.yu.filippova@gmail.com>
2023-04-04 14:24:01 +0300

    add tasks from form

--- src/components/createTaskForm/createTaskForm.js
+++ src/components/createTaskForm/createTaskForm.js
@@ -1,61 +1,44 @@
 import React, { useState } from "react";
-
-
 import { Component } from "react";
 
-class  CreateTaskForm extends Component{
-  constructor(props){
+class CreateTaskForm extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      title:'',
-      status:'todo'
-
-    }
+      title: "",
+      status: "todo",
+      
+    };
   }
-
   onValueChange = (e) => {
     this.setState({
-        [e.target.title] : e.target.value
-    })
-}
-
-onSubmit = (e) => {
+      [e.target.title]: e.target.value,
+    });
+  };
+  onSubmit = (e) => {
     e.preventDefault();
- 
 
-    this.props.onAdd(this.state.title, this.state.status);
+    this.props.addItem(this.state.title, this.state.status);
     this.setState({
-        title: '',
-       
-    })
-}
+      title: "",
+    });
+  };
+  render() {
+    const { title, status } = this.state;
 
-render(){
-  const {title, status} = this.state;
-  
-  return(
-    <form onSubmit={this.onSubmit} >
-      <input type="text" 
-      placeholder="add task"
-      name="name"
-      title="title"
-      value={title}
-      onChange={this.onValueChange}/>
-      <button type="submit" >Submit</button>
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="add task"
+          name="name"
+          title="title"
+          value={title}
+          onChange={this.onValueChange}
+        />
+        <button type="submit">Submit</button>
       </form>
-
-  )
+    );
+  }
 }
-
-}
-
-// function CreateTaskForm(){
-//   return(
-//     <form >
-//       <input type="text" id='title'  />
-//       <button >Submit</button>
-//       </form>
-//   )
-// }
-
 export default CreateTaskForm;
